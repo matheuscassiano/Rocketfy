53e2e2d09c31d3488ce16f44a99354c745455ef5
Matheus Cassiano <matheuscassiano9@gmail.com>
2020-04-20 13:45:20 -0300

    fisrt commit

---
+++ src/components/Header/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 80px;
+  padding: 0 30px;
+  background: #7159C1;
+  color: #FFF;
+
+  display: flex;
+  aling-items: center;
+`;
