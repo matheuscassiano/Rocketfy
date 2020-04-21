510be366c0512b23456fefa5f9f8dd339f74bf05
Matheus Cassiano <matheuscassiano9@gmail.com>
2020-04-21 10:58:35 -0300

    Ending Drag n' Drop

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   height: 80px;
-  padding: 0 30px;
+  padding: 35px 0 0 30px;
   background: #7159C1;
   color: #FFF;
 
